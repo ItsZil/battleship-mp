d58e5fc1e4c2cb8f7d08ce5e091116177c631c6e
Žilvinas Krasauskas <zilvinas.krasauskas@unity3d.com>
2023-10-05 18:46:59 +0300

    Move button colors to a struct

---
+++ SharedLibrary/Structs/ButtonColors.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace SharedLibrary.Structs
+{
+    public struct ButtonColors
+    {
+        public static Color Empty = Color.Transparent;
+        public static Color Ship = Color.Navy;
+        
+        public static Color Miss = Color.LightGray;
+        public static Color Hit = Color.Red;
+        public static Color Sunk = Color.DarkRed;
+    }
+}
