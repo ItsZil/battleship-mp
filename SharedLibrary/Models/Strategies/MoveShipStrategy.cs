26760f55a332318a02d633d3c8c991c889bc99dc
elzpil <elze.pilibaityte@ktu.edu>
2023-10-10 20:12:46 +0300

    added strategy pattern

---
+++ SharedLibrary/Models/Strategies/MoveShipStrategy.cs
@@ -0,0 +1,21 @@
+﻿using SharedLibrary.Interfaces;
+using SharedLibrary.Structs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Models.Strategies
+{
+    internal class MoveShipStrategy : IBoardCellInteractionStrategy
+    {
+        public HitDetails Interact(Game game, Shot shot)
+        {
+            var hitDetails = new HitDetails(shot.X, shot.Y);
+
+            return hitDetails;
+        }
+    {
+    }
+}
