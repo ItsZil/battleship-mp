45d2786ff273d9a82d7e9b70f8c1bc495306239f
Ostinas <ostinasmaciulis@gmail.com>
2023-11-08 00:25:55 +0200

    Added radar builder and partial placement functionality

---
+++ SharedLibrary/Models/Builders/RadarBuilder.cs
@@ -0,0 +1,40 @@
+﻿
+namespace SharedLibrary.Models.Builders
+{
+    public class RadarBuilder
+    {
+        private Radar radar;
+
+        public RadarBuilder()
+        {
+
+        }
+
+        public RadarBuilder AddCoordinate(int x, int y)
+        {
+            radar.Coordinates = new Coordinate(x, y);
+            return this;
+        }
+
+        public RadarBuilder AddHealth()
+        {
+            radar.Health = 1;
+            return this;
+        }
+
+        public RadarBuilder Build(string clientId)
+        {
+            radar = new Radar
+            {
+                RadarId = new Random().Next(1000, 9999),
+                PlayerId = clientId,
+            };
+            return this;
+        }
+
+        public Radar GetRadar()
+        {
+            return radar;
+        }
+    }
+}
---
+++ SharedLibrary/Models/Radar.cs
@@ -0,0 +1,16 @@
+﻿namespace SharedLibrary.Models
+{
+    public class Radar
+    {
+        public string PlayerId { get; set; }
+        public int RadarId { get; set; }
+        public int Health { get; set; }
+
+        public Coordinate Coordinates { get; set; }
+
+        public Radar()
+        {
+
+        }
+    }
+}
