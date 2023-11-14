1408232bcb72198a23642d191ee6b40ee3542dbb
Ostinas <ostinasmaciulis@gmail.com>
2023-11-14 16:27:22 +0200

    Added base project for unit testing

---
+++ UnitTests/ClientAppTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UnitTests.ClientAppTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ UnitTests/SharedLibraryTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UnitTests.SharedLibraryTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
