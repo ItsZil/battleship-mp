915f2c4ab19b7bfe84d86558731071cf56782730
Žilvinas Krasauskas <zilvinas.krasauskas@unity3d.com>
2023-11-15 20:25:16 +0200

    Exception tests

---
+++ UnitTests/SharedLibraryTests/ExceptionTests.cs
@@ -0,0 +1,48 @@
+﻿using SharedLibrary.Exceptions;
+
+namespace UnitTests.SharedLibraryTests
+{
+    [TestFixture]
+    public class ExceptionTests
+    {
+        [Test]
+        public void ErrorMessage_MessageIsCorrect()
+        {
+            ErrorMessage errorMessage = new ErrorMessage("Test");
+
+            Assert.That(errorMessage.Message == "Test");
+        }
+
+        [Test]
+        public void GameFullException_MessageIsCorrect()
+        {
+            GameFullException exception = new GameFullException();
+
+            Assert.That(exception.Message == "Game is full!");
+        }
+
+        [Test]
+        public void GameNotFound_MessageIsCorrect()
+        {
+            GameNotFoundException exception = new GameNotFoundException();
+
+            Assert.That(exception.Message == "Game not found!");
+        }
+
+        [Test]
+        public void InvalidPasswordException_MessageIsCorrect()
+        {
+            InvalidPasswordException exception = new InvalidPasswordException();
+
+            Assert.That(exception.Message == "Invalid password!");
+        }
+
+        [Test]
+        public void ServerNameTakenException_MessageIsCorrect()
+        {
+            ServerNameTakenException exception = new ServerNameTakenException();
+
+            Assert.That(exception.Message == "Server name taken!");
+        }
+    }
+}
