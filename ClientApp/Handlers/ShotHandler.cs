e1d8bca1b25c2ab5478f4fc6efd3162e430da3d0
Žilvinas Krasauskas <zilvinas.krasauskas@unity3d.com>
2023-11-26 23:41:22 +0200

    Implement chain of responsibility pattern for ship group shots

---
+++ ClientApp/Handlers/ShotHandler.cs
@@ -0,0 +1,17 @@
+﻿using SharedLibrary.Models;
+using SharedLibrary.Structs;
+
+namespace ClientApp.Handlers
+{
+    public abstract class ShotHandler
+    {
+        protected ShotHandler _nextHandler;
+
+        public void SetNextHandler(ShotHandler nextHandler)
+        {
+            _nextHandler = nextHandler;
+        }
+
+        public abstract HitDetails HandleShot(Shot shot, Client client);
+    }
+}
