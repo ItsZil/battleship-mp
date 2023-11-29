a950fee3838dd949b5f790abbcadf7d5638e78ff
Ostinas <ostinasmaciulis@gmail.com>
2023-11-29 20:59:22 +0200

    Added state pattern to manage visible UI elements depending on selected action

---
+++ ClientApp/State/States/ShootActionState.cs
@@ -0,0 +1,20 @@
+﻿namespace ClientApp.State.States
+{
+    public class ShootActionState : IActionState
+    {
+        public void DisableUIElements(List<Control> disableList)
+        {
+            foreach (var control in disableList)
+            {
+                control.Visible = false;
+            }
+        }
+        public void EnableUIElements(List<Control> enableList)
+        {
+            foreach (var control in enableList)
+            {
+                control.Visible = true;
+            }
+        }
+    }
+}
---
+++ ClientApp/State/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using ClientApp.State.States;
+using System.ComponentModel;
+
+namespace ClientApp.State
+{
+    public class UIManager
+    {
+        private IActionState selectedAction;
+
+        public UIManager()
+        {
+            selectedAction = new MoveActionState();
+        }
+
+        public void SetState (IActionState action)
+        {
+            selectedAction = action;
+        }
+
+        public void Disable(List<Control> disableList)
+        {
+            selectedAction.DisableUIElements(disableList);
+        }
+
+        public void Enable(List<Control> enableList)
+        {
+            selectedAction.EnableUIElements(enableList);
+        }
+    }
+}
