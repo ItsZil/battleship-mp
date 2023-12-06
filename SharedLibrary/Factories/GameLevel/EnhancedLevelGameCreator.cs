36c17ab75cb4267e021b9548a4ded1ff8f0b2257
TadisOn <76095383+TadisOn@users.noreply.github.com>
2023-12-06 14:27:39 +0200

    Added template design pattern

---
+++ SharedLibrary/Factories/GameLevel/EnhancedLevelGameCreator.cs
@@ -0,0 +1,29 @@
+﻿using SharedLibrary.Models;
+using SharedLibrary.Models.Levels;
+using SharedLibrary.Templates;
+
+namespace SharedLibrary.Factories.GameLevel
+{
+    public class EnhancedLevelGameCreator : GameCreationTemplate
+    {
+
+        public EnhancedLevelGameCreator(string levelName) : base(levelName) { }
+
+        protected override Game InitializeGame(string creatorId, string serverName, string password, string levelName, List<Player> players)
+        {
+            return new AdvancedGame(creatorId, serverName, password, levelName, players);
+        }
+
+        protected override bool GetSupportsAllShips() => true;
+        protected override bool GetSupportsMovingShips() => false;
+        protected override bool GetSupportRadars() => false;
+
+        public string GetLevelName()
+        {
+            return levelName;
+        }
+    }
+}
+
+    
+
---
+++ SharedLibrary/Templates/GameCreationTemplate.cs
@@ -0,0 +1,44 @@
+﻿using SharedLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Templates
+{
+    public abstract class GameCreationTemplate
+    {
+        protected string levelName;
+
+        public GameCreationTemplate(string levelName)
+        {
+            this.levelName = levelName;
+        }
+
+        public Game CreateGame(string creatorId, string serverName, string password)
+        {
+            var players = new List<Player>
+            {
+                new Player(creatorId, "Player 1")
+            };
+
+            var game = InitializeGame(creatorId, serverName, password, levelName, players);
+
+            game.SupportsAllShips = GetSupportsAllShips();
+            game.SupportsMovingShips = GetSupportsMovingShips();
+            game.SupportsRadars = GetSupportRadars();
+
+            return game;
+
+        }
+
+        protected abstract Game InitializeGame(string creatorId, string serverName, string password, string levelName, List<Player> players);
+
+        protected virtual bool GetSupportsAllShips() => false;
+        protected virtual bool GetSupportsMovingShips() => false;
+
+        protected virtual bool GetSupportRadars() => false;
+
+    }
+}
